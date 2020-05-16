435de1775e068e203a4e8162bfe15137b69f2c07
Ricardo Ruiz <gaanzz11@gmail.com>
2020-05-15 21:02:28 -0500

    Structs and methods

---
+++ method.go
@@ -0,0 +1,18 @@
+package main
+
+import("fmt"
+	 "math")
+
+
+type Circle struct {
+	x, y, r float64
+}
+
+func (c *Circle) circleArea() float64  {
+	return math.Pi * c.r * c.r
+}
+
+func main() {
+	c := Circle{0,0,5}
+	fmt.Println(c.circleArea())
+}
